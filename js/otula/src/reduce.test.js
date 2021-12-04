52066b0401485d5282b5ed3bd021370059ec76ce
styrmank-mainUser <styrmank-mainuser@styrmank-debian.styrmank>
2021-12-04 12:33:39 +0200

    Updated reduce.test.js

--- js/otula/src/reduce.test.js
+++ js/otula/src/reduce.test.js
@@ -1,8 +1,8 @@
 import reduce from "./reduce"
 
 
-describe(”Test reduce”, () => {
-	test(”should reduce correctly”, () => {
-		expect(isEmpty([1,2], (sum, n) => sum + n, 0)).toEqual(3)
+describe("Test reduce", () => {
+	test("should reduce correctly", () => {
+		expect(reduce([1,2], (sum, n) => sum + n, 0)).toEqual(3)
 	})
 })
