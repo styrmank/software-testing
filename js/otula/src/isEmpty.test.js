16709d4f99202e6f4f8c41ab1beb840ede4dd2e5
hnigee <heinigee@Heini-MacBook-Pro.local>
2021-12-04 10:32:25 +0200

    added reduce and fixed missing  bracket in isEmpty

--- js/otula/src/isEmpty.test.js
+++ js/otula/src/isEmpty.test.js
@@ -3,6 +3,6 @@ Const subtract = require(”./isEmpty”)
 
 describe(”Test if is empty”, () => {
 	test(”should tell if empty”, () => {
-		expect(isEmpty(1).toEqual(true)
+		expect(isEmpty(1)).toEqual(true)
 	})
 })
---
+++ js/otula/src/reduce.test.js
@@ -0,0 +1,8 @@
+Const subtract = require(”./reduce”)
+
+
+describe(”Test reduce”, () => {
+	test(”should reduce correctly”, () => {
+		expect(isEmpty(3,1)).toEqual(2)
+	})
+})
