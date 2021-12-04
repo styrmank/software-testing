d38772e220f71f10a382781eb75f9247d0927d85
styrmank-mainUser <styrmank-mainuser@styrmank-debian.styrmank>
2021-12-04 12:59:00 +0200

    Added .gitignore. Added test for eq.js

---
+++ js/otula/src/eq.test.js
@@ -0,0 +1,13 @@
+import eq from "./eq"
+
+
+test("Test eq.js", () => {
+    const first = {"value" : 1}
+    const second = {"value" : 1}
+
+    const a1 = 'a'
+    const a2 = 'a'
+
+    expect(eq(first,second)).toEqual(false)
+    expect(eq(a1, a2)).toEqual(true)
+})
