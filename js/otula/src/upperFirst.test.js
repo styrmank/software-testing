8ffad2da49919a98e287e45409e435671c4fcfef
hnigee <heinigee@Heini-MacBook-Pro.local>
2021-12-04 12:20:35 +0200

    fixed upperFirst

--- js/otula/src/upperFirst.test.js
+++ js/otula/src/upperFirst.test.js
@@ -1,8 +1,7 @@
 import upperFirst from "./upperFirst"
 
 
-describe(”Test upperFirst”, () => {
+
 	test(”should check if first character is capital”, () => {
 		expect(upperFirst("Testisana")).toEqual(true)
 	})
-})
