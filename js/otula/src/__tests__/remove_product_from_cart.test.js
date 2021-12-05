5bf4d9a2bb3456a6b8637b1b56be82641ede5a6c
styrmank-mainUser <styrmank-mainuser@styrmank-debian.styrmank>
2021-12-05 17:53:07 +0200

    All ready

---
+++ js/otula/src/__tests__/remove_product_from_cart.test.js
@@ -0,0 +1,17 @@
+/**
+ * Remove product from cart.
+ * Products can be removed from the shopping cart and the total 
+ * sum of the products in cart is updated to match the products left
+ * in the cart.
+ */
+
+s
+
+
+ import {Store,Cart} from '../testing_utils/products.js'
+
+
+ describe("Test #6: Remove product from cart", () => {
+     
+     
+ })
